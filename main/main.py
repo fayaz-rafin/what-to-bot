bd7803457d51ebb45560a29b89f3e4bf6f338291
Fayaz Rafin <fayaz.rafin@gmail.com>
2022-05-08 02:11:25 -0400

    Added a Help command for users to know what commands they can use
    Created a $Help command

--- main/main.py
+++ main/main.py
@@ -20,26 +20,9 @@ async def on_ready():
     print("I'm powered up and ready to go!")
 
 
-#bs-------------------------------------------------------------------------
-
-
-# return(moviee)
-@client.command()
-async def movie(ctx, message):
-  response = requests.get("http://img.omdbapi.com/?apikey=[yourkey]&")
-
-  json_data = json.loads(response.text)
-  quote = json_data[0]['q'] + " -" + json_data[0]['a']
-  moviee = json_data[0]
-  await message.ctx.send(moviee)
-
-
-#bs-------------------------------------------------------------------------
-
-
 @client.command()
 async def wheretogo(ctx, message):
-    with open('main/places.json') as f:
+    with open('places.json') as f:
         places = json.load(f)
         ran = random.randint(0, 5)
         name = places[f'{message}'][ran]["name"]
@@ -51,8 +34,21 @@ async def wheretogo(ctx, message):
 
 
 @client.command()
+async def whattowatch(ctx):
+    with open('movie.json') as f:
+        movie = json.load(f)
+        ran = random.randint(0, 2)
+        name = movie["Movie"][ran]["name"]
+        pic = movie["Movie"][ran]["pic"]
+        descrip = movie["Movie"][ran]["description"]
+        embed = discord.Embed(title=name, description=descrip, color=0xFF5733)
+        embed.set_thumbnail(url=pic)
+        await ctx.send(embed=embed)
+
+
+@client.command()
 async def whattocook(ctx, message):
-    with open('main/food.json') as f:
+    with open('food.json') as f:
         data = json.load(f)
         ran = random.randint(0, 3)
         name = data[f'{message}'][ran]["name"]
@@ -68,10 +64,34 @@ async def whattocook(ctx, message):
             color=0xfce188)
         #embed.set_thumbnail(url=pic)
         embed.set_image(url=pic)
+      
         embed.add_field(name=serving, value=url, inline=False)
         #embed.set_footer(text='blah blah')
 
     await ctx.send(embed=embed)
 
+@client.command()
+async def whattolisten(ctx):
+    with open('music.json') as f:
+      music = json.load(f)
+      ran = random.randint(0,3)
+      name = music["Music"][ran]["name"]
+      artist = music["Music"][ran]["artist"]
+      url = music["Music"][ran]["url"]
+      embed = discord.Embed(title=name,url=url,description=artist,color=0xfce188)
+      embed.add_field(name="Song URL:", value=url, inline=False)
+      #embed.set_image(url=url)
+      await ctx.send(embed=embed)
+
+@client.command()
+async def Help(ctx):
+  embed = discord.Embed(title="Bot Commands",description="Here's what i can do: Type 'whattocook {Food, Dessert, Drink}' for food reccomendations. Type 'whattowatch' to watch something when you're bored to find show reccomendations. Type 'whattolisten' to get song reccomendations and spice up your playlist. Type 'wheretogo' to get help deciding where to plan your next hangout", color=0xfce188)
+  
+  
+  await ctx.send(embed=embed)
+  
+
+      
+      
 
 client.run(os.getenv("TOKEN"))
